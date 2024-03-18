a8b3bc6e37369f41060e7c7ec0a9e85f85f6bcb5
Braeden Lhamon <S543387@nwmissouri.edu>
2024-03-18 17:53:53 -0500

    Trying to add our game to the webserver

--- se-website/src/App.js
+++ se-website/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import precipGameLink from '.\games\precipitation_game\Precipitation Minigame.html'
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Jack Kelley is super duper awesome!</p>
+        <p>Click the link below to play my Godot game:</p>
+      <GodotGameLink />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ se-website/src/games/precipitation_game/precipGameLink.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const precipGameLink = () => {
+  return (
+    <a href={process.env.PUBLIC_URL + 'C:\Users\S543387\Documents\GitHub\blhamon055.github.io\se-website\src\games\precipitation_game\Precipitation Minigame.html'} target="_blank" rel="noopener noreferrer">
+      Play My Godot Game
+    </a>
+  );
+};
+
+export default GodotGameLink;
