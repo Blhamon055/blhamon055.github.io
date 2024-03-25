89d3afaa4238a94d8c653f61f2edf14c14de5fda
Jakelayman0110 <99349809+Jakelayman0110@users.noreply.github.com>
2024-03-25 14:31:07 -0500

    worked on userlogin.js

---
+++ se-website/src/UserLoginComponent/userlogin.js
@@ -0,0 +1,14 @@
+import "./userlogin.css";
+
+function loginPage() {
+  return (
+    <div className="loginPage">
+      <header className="loginPage-header">
+        <p>Jack Kelley is super duper punny!</p>
+        <p>Testing to see if this page loads!</p>
+      </header>
+    </div>
+  );
+}
+
+export default loginPage();
