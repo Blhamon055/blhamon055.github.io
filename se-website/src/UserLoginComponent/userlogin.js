e0eea41cb4fee6231aed1ca3610a6175755e5c11
Corben McClintock <s544086@nwmissouri.edu>
2024-04-03 14:39:46 -0500

    Adding in login page

--- se-website/src/UserLoginComponent/userlogin.js
+++ se-website/src/UserLoginComponent/userlogin.js
@@ -1,14 +1,27 @@
 import "./userlogin.css";
+import React, { useState } from 'react';
 
-function loginPage() {
+const LoginPage = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="loginPage">
-      <header className="loginPage-header">
-        <p>Jack Kelley is super duper punny!</p>
-        <p>Testing to see if this page loads!</p>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+      </div>
+      <button type="submit">Login</button>
+    </form>  
   );
-}
+};
 
-export default loginPage();
+export default LoginPage();
