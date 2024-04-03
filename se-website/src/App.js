50ae71cdcf882712baebd42bcae910e397f96623
Braeden Lhamon <S543387@nwmissouri.edu>
2024-04-03 14:19:25 -0500

    Removed parts from the app.js and created login button component

--- se-website/src/App.js
+++ se-website/src/App.js
@@ -1,25 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import PrecipGameLink from './games/precipitation_game/precipGameLink';
+import ButtonLink from './AdminLoginComponent/adminbutton'
 import AdminLoginForm from "./AdminLoginComponent/adminloginformtest";
 import userlogin from "./UserLoginComponent/userlogin";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Jack Kelley is super duper punny!</p>
-        <p>Click the link below to play my Godot game:</p>
-        <AdminLoginForm />
-      <PrecipGameLink />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <ButtonLink to="/adminlogin">Admin Login!</ButtonLink>
       </header>
     </div>
   );
--- se-website/src/index.js
+++ se-website/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import adminbutton from './AdminLoginComponent/adminbutton'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import AdminLoginPage from './AdminLoginComponent/adminloginpagetest';
 
