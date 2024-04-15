3bc38a6d90eb4f1ca94390343011817853a581e2
spencera0024 <spencera024@gmail.com>
2024-04-15 14:52:52 -0500

    Images display the name of game when hovered over

--- src/IconsComponent/icons.js
+++ src/IconsComponent/icons.js
@@ -1,10 +1,42 @@
 import React, { useState } from 'react';
+import acidBaseImage from "./acidbase.png"
+import preciptationImage from "./preciptation.png"
 
 const Icons = ({text, effectType}) => {
+
+    const [hover, setHover] = useState(false);
+
+    const onHover = (x) => {
+        x.preventDefault();
+        setHover(true);
+    };
+
+    const onHoverOver = (x) => {
+        x.preventDefault();
+        setHover(false);
+    };
+
     return (
-    <button style={{ margin: "10px" }} className={`custom-button ${effectType}`}>
-        {text}
-    </button>
+        <div>
+            <div id="1">
+            {hover && <p>Acid Base Game</p>}
+            <img 
+                onMouseEnter={(x) => onHover(x)}
+                onMouseLeave={(x) => onHoverOver(x)}
+                src = {acidBaseImage}
+                style={{width: 300}, {height: 300}}
+            />
+            </div>
+        <div id="2">
+            {hover && <p>Preciptation Game</p>}
+            <img
+                onMouseEnter={(x) => onHover(x)}
+                onMouseLeave={(x) => onHoverOver(x)}
+                src = {preciptationImage}
+                style={{width: 300}, {height: 300}}
+            />
+        </div>
+        </div>
     )
 };
 
